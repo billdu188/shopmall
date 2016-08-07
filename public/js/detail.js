b2b19e0c913063bf5ae2d91f62051dfa7bb6e171
dubingqing <billdu188@gmail.com>
2016-08-07 13:31:57 +0800

    adjust detail

--- public/js/detail.js
+++ public/js/detail.js
@@ -21,4 +21,25 @@ $(function(){
 		})
 	})
 	
+	$('.order-btn').click(function() {
+		
+		$.ajax({
+			type: 'POST',
+			url: '/shopcar',
+			data: {cakeId: cakeId},
+			dataType: 'json'
+		})
+		.done(function(results) {
+
+			if (results.success === 1) {
+				//商品总数
+				$('.badge').html(results.sumGoods)
+				//总价
+				$('.sumValue').html(results.sumValues+'元')
+
+				//跳转
+				window.location.href="/order"
+			}
+		})
+	})
 }) 
